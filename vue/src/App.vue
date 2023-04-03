89d4c8ff02fcdae08e179662dcd4a776a88d01f8
melinamui <113132371+melinamui@users.noreply.github.com>
2023-04-02 23:57:44 -0400

    Update App.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,8 @@
+<!--
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-</script>
+</script>-->
 
 <template>
   <h1>Menu</h1>
