463086f408189451d008871f9438f3eb9185613a
melinamui <113132371+melinamui@users.noreply.github.com>
2023-04-03 00:16:14 -0400

    testing

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!--<script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
@@ -7,3 +7,4 @@ import TheWelcome from '../components/TheWelcome.vue'
     <TheWelcome />
   </main>
 </template>
+-->
