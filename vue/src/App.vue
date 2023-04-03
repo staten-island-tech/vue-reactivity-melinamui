8fa36152ed82cbcb49eb90e822c75ba86095a3d8
melinamui <113132371+melinamui@users.noreply.github.com>
2023-04-02 23:55:40 -0400

    Update App.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -9,6 +9,9 @@ import HelloWorld from './components/HelloWorld.vue'
   <div v-for="menu in menu"
   :key="menu.name">
   {{ menu.name }}
+  <img :src="menu.image"/>
+  {{ menu.cost }}
+  <button>Add to Cart</button>
   </div>
 </div>
 </template>
@@ -21,52 +24,65 @@ export default {
     menu : [
 {
   name: 'Salmon Maki',
-  image: '../images/salmon roll.jpg'
+  image: '../images/salmon roll.jpg',
+  cost: "1 Soul",
 },
 {
   name: 'Tuna Maki',
-  image: '../images/tuna roll.jpg'
+  image: '../images/tuna roll.jpg',
+  cost: "1 Soul"
 },
 {
   name: 'Tuna Nigiri',
-  image: '../images/tuna roll.jpg'
+  image: '../images/tuna roll.jpg',
+  cost: "1 Soul"
 },
-{ name: 'Tamago Nigiri', image: '../images/tamago nigiri.jpg' },
+{ name: 'Tamago Nigiri', image: '../images/tamago nigiri.jpg',
+cost: "1 Soul" },
 {
   name: 'Shrimp Nigiri',
-  image: '../images/shrimp nigiri.jpg'
+  image: '../images/shrimp nigiri.jpg',
+  cost: "1 Soul"
 },
 {
   name: 'Roe Nigiri',
-  image: '../images/roe .jpg'
+  image: '../images/roe .jpg',
+  cost: "1 Soul"
 },
 {
   name: 'Salmon Sashimi',
-  image: '../images/salmon sashimi.jpg'
+  image: '../images/salmon sashimi.jpg',
+  cost: "1 Soul"
 },
 {
   name: 'Tuna Sashimi',
-  image: '../images/tuna sashimi.jpg'
+  image: '../images/tuna sashimi.jpg',
+  cost: "1 Soul"
 },
 {
   name: 'Tamago',
-  image: '../images/tamago .jpg'
+  image: '../images/tamago .jpg',
+  cost: "1 Soul"
 },
 {
   name: 'Salmon Uramaki',
-  image: '../images/salmon uramaki.jpg'
+  image: '../images/salmon uramaki.jpg',
+  cost: "1 Soul"
 },
 {
   name: 'Shrimp Uramki',
-  image: '../images/shrimp uramaki.jpg'
+  image: '../images/shrimp uramaki.jpg',
+  cost: "1 Soul"
 },
 {
   name: 'Avocado and Salmon Temaki',
-  image: '../images/avocade and salmon temaki.jpg'
+  image: '../images/avocade and salmon temaki.jpg',
+  cost: "1 Soul"
 },
 {
   name: 'Tuna Temaki',
-  image: '../images/tuna hand roll.jpg'
+  image: '../images/tuna hand roll.jpg',
+  cost: "1 Soul"
 }
 ]
   }
