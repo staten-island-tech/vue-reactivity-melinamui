826a18b1adfbe2e47199396728770bf489d79d85
melinamui <113132371+melinamui@users.noreply.github.com>
2023-04-02 23:49:17 -0400

    created menu

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,6 +3,11 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+
+
+
+
+<!--
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
@@ -83,3 +88,4 @@ nav a:first-of-type {
   }
 }
 </style>
+-->
